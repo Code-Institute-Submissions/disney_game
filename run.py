1aa506794d8f33b2d5d1182b74f84cb255ac54a3
Simon Tidman <simon.tid@btinternet.com>
2018-12-22 21:14:49 +0000

    basic routes and pages added

--- run.py
+++ run.py
@@ -1,11 +1,20 @@
 import os 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "<h1>Hello</h1>"
+    return render_template("index.html")
+    
+@app.route('/user')
+def user():
+    return render_template("user.html")
+
+@app.route('/user/<name>')
+def username(name):
+     return render_template("game.html",name=name)
     
 app.run(host=os.getenv('IP'), port=int (os.getenv('PORT')), debug=True)
-    
+
+
