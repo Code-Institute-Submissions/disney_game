6e0b6501700d4ac19ff4d5b8c460625c4377a378
Simon Tidman <simon.tid@btinternet.com>
2018-12-24 21:33:58 +0000

    styled navbar

--- run.py
+++ run.py
@@ -1,9 +1,9 @@
 import os 
 from flask import Flask, render_template
-from flask_bootstrap import Bootstrap
+
 
 app = Flask(__name__)
-bootstrap = Bootstrap(app)
+
 @app.route('/')
 def index():
     return render_template("index.html")
