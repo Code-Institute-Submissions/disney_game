337d08ca7265b7848613fb3b72d52b3ab14e2e8d
Simon Tidman <simon.tid@btinternet.com>
2019-01-02 22:28:19 +0000

    routes established

--- run.py
+++ run.py
@@ -1,21 +1,31 @@
 import os 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, session
 
 
 app = Flask(__name__)
+app.secret_key = "randomstring123"
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    return render_template("index.html")
-    
-@app.route('/user')
+        return render_template('index.html')
+
+@app.route("/user",methods=["GET","POST"])
 def user():
+    if request.method == "POST":
+        
+        session["username"] = request.form["username"]
+
+    if "username" in session:
+        return redirect(session["username"])
     return render_template("user.html")
 
-@app.route('/user/<name>')
-def username(name):
-     return render_template("game.html",name=name)
-    
+@app.route("/<username>")
+def users(username):
+   
+    return render_template("game.html", username=username)       
+        
 app.run(host=os.getenv('IP'), port=int (os.getenv('PORT')), debug=True)
+
 
 
