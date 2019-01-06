424536723d41ce1e15c38a7acb8da816d26d8e37
Simon Tidman <simon.tid@btinternet.com>
2019-01-06 21:24:11 +0000

    test framework added and answer check added

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ import os
 import json
 from flask import Flask, render_template, request, redirect, session
 
-
+guesses = [] 
 app = Flask(__name__)
 app.secret_key = "randomstring123"
 
@@ -21,12 +21,26 @@ def user():
         return redirect(session["username"])
     return render_template("user.html")
 
-@app.route("/<username>")
+@app.route("/<username>", methods=["GET","POST"])
+
 def users(username):
     data = []
+    score = 0
+    image_counter=0
+     
     with open("data/guess_data.json", "r") as json_data:
         data = json.load(json_data)
-    return render_template("game.html", username=username, guess_data=data)       
+        answer= data[image_counter]['answer'] 
+  
+        if request.method == "POST":
+            
+            add=request.form['guess']
+            if add == answer:
+             return("correct")
+            else:    
+             guesses.append(add)
+            
+    return render_template("game.html", username=username, guess_data=data, score=score, guesses=guesses, counter=image_counter)       
         
 app.run(host=os.getenv('IP'), port=int (os.getenv('PORT')), debug=True)
 
---
+++ test_run.py
@@ -0,0 +1,6 @@
+import unittest
+
+class test_disney(unittest.TestCase):
+    
+    def test_isthisworking(self):
+       self.assertEqual(1,1)
